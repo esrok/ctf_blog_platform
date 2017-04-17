c7b61001f282440e70a41d00500363debf4af538
korse <korse@seclab.cs.msu.su>
2017-04-17 21:40:31 +0300

    Use `text_preview` instead of `text` in posts lists
    posts lists should not blow up because of large texts

--- blog_platform/models.py
+++ blog_platform/models.py
@@ -7,6 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class BlogPost(models.Model):
     TITLE_LENGTH = 40
+    TEXT_PREVIEW_LENGTH = 100
+    PREVIEW_SUFFIX = '...'
+
     title = models.CharField(max_length=TITLE_LENGTH, blank=False, null=False)
     slug = models.SlugField(max_length=TITLE_LENGTH)
     text = models.TextField(blank=False)
@@ -19,6 +22,12 @@ class BlogPost(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(BlogPost, self).save(*args, **kwargs)
+
+    @property
+    def text_preview(self):
+        if len(self.text) <= self.TEXT_PREVIEW_LENGTH:
+            return self.text
+        return self.text[:self.TEXT_PREVIEW_LENGTH - len(self.PREVIEW_SUFFIX)] + self.PREVIEW_SUFFIX
 
     def __unicode__(self):
         return self.title
